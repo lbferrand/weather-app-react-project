1bda383c5d81158754eb3bb9ab08939b4d537bb6
Luciana B Ferrand <lbferrand@gmail.com>
2022-01-18 10:42:30 -0500

    Installed react dependencies

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <footer className="footer">
+        <p>This site has been coded by Luciana B Ferrand</p>
+        <a
+          className="Luciana Ferrand-link"
+          href="https://www.lbferrandportfolio.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My website
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
