8bc01ebf15db547a7acdacfe060e612a729e20dc
Luciana B Ferrand <lbferrand@gmail.com>
2022-01-18 12:24:28 -0500

    Added bootstrap & imported

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer className="footer">
-        <p>This project was coded by Luciana B Ferrand</p>
-        <a
-          className="Luciana Ferrand-link"
-          href="https://www.lbferrandportfolio.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My website
-          <br />
+      <div className="container">
+        <footer className="footer">
+          <p>This project was coded by Luciana B Ferrand</p>
           <a
-            className="github-link"
-            href="https://github.com/lbferrand/weather-app-react-project"
+            className="Luciana Ferrand-link"
+            href="https://www.lbferrandportfolio.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Open-sourced on GitHub
+            My website
+            <br />
+            <a
+              className="github-link"
+              href="https://github.com/lbferrand/weather-app-react-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-sourced on GitHub
+            </a>
           </a>
-        </a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
